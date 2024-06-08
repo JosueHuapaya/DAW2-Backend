c03af807c0280591f27d0f9fb6847517ddfc2fcf
Victor Borja <vborja2424@gmail.com>
2024-06-08 17:40:18 -0500

    Avance para PC1

---
+++ src/main/java/com/prestamo/repository/CoordenadaRepository.java
@@ -0,0 +1,10 @@
+package com.prestamo.repository;
+
+import com.prestamo.entity.Coordenada;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoordenadaRepository  extends JpaRepository<Coordenada, Integer> {
+
+
+
+}
