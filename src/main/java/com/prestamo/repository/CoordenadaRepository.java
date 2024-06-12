7be8951d3c62f47c1d0ed4f1f2e929f08a06e7f9
Victor Borja <vborja2424@gmail.com>
2024-06-12 00:01:30 -0500

    terminado

--- src/main/java/com/prestamo/repository/CoordenadaRepository.java
+++ src/main/java/com/prestamo/repository/CoordenadaRepository.java
@@ -1,10 +1,15 @@
 package com.prestamo.repository;
 
 import com.prestamo.entity.Coordenada;
+import com.prestamo.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CoordenadaRepository  extends JpaRepository<Coordenada, Integer> {
 
-
+    @Query("Select r from Usuario r, UsuarioHasRol u where r.idUsuario = u.usuario.idUsuario and u.rol.idRol = 4 order by r.apellidos desc ")
+    public abstract List<Usuario> listaPrestamistariosTotales();
 
 }
