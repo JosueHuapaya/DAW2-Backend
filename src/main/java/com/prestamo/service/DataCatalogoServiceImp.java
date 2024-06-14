a4b83636083bfe12162e1d9464509932d77bba7b
USUARIO <lourdes.99101@gmail.com>
2024-06-14 14:06:06 -0500

    PC1-terminado

--- src/main/java/com/prestamo/service/DataCatalogoServiceImp.java
+++ src/main/java/com/prestamo/service/DataCatalogoServiceImp.java
@@ -2,6 +2,8 @@ package com.prestamo.service;
 
 import java.util.List;
 
+import com.prestamo.entity.Catalogo;
+import com.prestamo.entity.Ejemplo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +12,21 @@ import com.prestamo.repository.DataCatalogoRepository;
 
 @Service
 public class DataCatalogoServiceImp implements DataCatalogoService {
-
 	@Autowired
 	private DataCatalogoRepository repository;
-
 	@Override
 	public List<DataCatalogo> listaDataCatalogo(int idTipo) {
 		return repository.listaDataCatalogo(idTipo);
 	}
+	@Override
+	public DataCatalogo registrar(DataCatalogo dataCatalogo) {
+		return repository.save(dataCatalogo);
+	}
+
+	@Override
+	public List<DataCatalogo> listaDataCatalogoPorDescripcionIgual(String descripcion) {
+		return repository.listaDataCatalogoPorDescripcionIgual(descripcion);
+	}
+
 
 }
