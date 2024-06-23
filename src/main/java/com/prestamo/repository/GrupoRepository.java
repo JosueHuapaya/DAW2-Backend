c61eef794957b8505ac57c6b01bf084d6be84e89
JosueHuapaya <josuehuapaya@hotmail.com>
2024-06-23 10:27:20 -0500

    Examen terminado PC2-Huapaya

--- src/main/java/com/prestamo/repository/GrupoRepository.java
+++ src/main/java/com/prestamo/repository/GrupoRepository.java
@@ -11,4 +11,10 @@ public interface GrupoRepository extends JpaRepository<Grupo, Integer> {
 
 	@Query("select e from Grupo e where e.descripcion = ?1")
 	public abstract List<Grupo> listaGrupoPorDescripcionIgual(String descripcion);
+	
+	@Query("select e from Grupo e where e.descripcion like ?1")
+	public abstract List<Grupo> listaGrupoPorDescripcionLike(String descripcion);
+	
+	@Query("select e from Grupo e where e.descripcion = ?1 and e.idGrupo != ?2 ")
+	public abstract List<Grupo> listaGrupoPorDescripcionIgualActualiza(String descripcion, int idEjemplo);
 }
