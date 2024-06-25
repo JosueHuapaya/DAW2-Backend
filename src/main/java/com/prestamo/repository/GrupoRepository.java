40dc3ddf634c65dec20c2bd70db8aa5559b0d403
JosueHuapaya <josuehuapaya@hotmail.com>
2024-06-24 21:00:51 -0500

    EF-Huapaya GrupoConsulta terminado

--- src/main/java/com/prestamo/repository/GrupoRepository.java
+++ src/main/java/com/prestamo/repository/GrupoRepository.java
@@ -1,7 +1,7 @@
 package com.prestamo.repository;
 
-import java.util.List;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -17,4 +17,13 @@ public interface GrupoRepository extends JpaRepository<Grupo, Integer> {
 	
 	@Query("select e from Grupo e where e.descripcion = ?1 and e.idGrupo != ?2 ")
 	public abstract List<Grupo> listaGrupoPorDescripcionIgualActualiza(String descripcion, int idEjemplo);
+	
+	
+	
+	@Query("SELECT e FROM Grupo e where "
+	        + " e.descripcion like ?1  and "
+	        + " (?2 = -1 or e.usuarioLider.idUsuario = ?2) and "
+	        + " e.estado = ?3 ")
+public abstract List<Grupo> listaConsultaCompleja(String descripcion, int idUsuarioLider, int estado);
+	
 }
