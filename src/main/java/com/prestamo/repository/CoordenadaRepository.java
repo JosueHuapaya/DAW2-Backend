ba1258a9a97f9a0ac7866460ec091ba3e8d300ca
vborja21 <vborja2424@gmail.com>
2024-06-25 16:00:42 -0500

    Terminado PC2

--- src/main/java/com/prestamo/repository/CoordenadaRepository.java
+++ src/main/java/com/prestamo/repository/CoordenadaRepository.java
@@ -1,15 +1,20 @@
 package com.prestamo.repository;
 
 import com.prestamo.entity.Coordenada;
+import com.prestamo.entity.MontoPrestamo;
 import com.prestamo.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface CoordenadaRepository  extends JpaRepository<Coordenada, Integer> {
 
     @Query("Select r from Usuario r, UsuarioHasRol u where r.idUsuario = u.usuario.idUsuario and u.rol.idRol = 4 order by r.apellidos desc ")
     public abstract List<Usuario> listaPrestamistariosTotales();
+
+    @Query("SELECT r FROM Coordenada r WHERE CAST(r.latitud AS string) LIKE CONCAT(?1, '%')")
+    List<Coordenada> obtenerCoordenadasPorPrimerosDigitosLatitud(String latitudDigito);
 
 }
